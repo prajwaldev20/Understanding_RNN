dff22b9d1cad428a9ea9c3b60021333ceefd58db
Prajwal Rudresh <prajwal.rudresh@gmail.com>
2025-08-02 23:15:20 -0500

    Fixed error

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.models import load_model
 
-# Load the IMDB dataset word index from local JSON file
-with open('imdb_word_index.json', 'r') as f:
-    word_index = json.load(f)
 
 # Load the IMDB dataset word index
 word_index = imdb.get_word_index()
